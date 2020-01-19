9247143609f47510f1a132f5c74d471d52e759d2
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2020-01-18 21:26:34 -0500

    Added initial library to handle DB access

---
+++ HammerAndStain.Projects.CostCalculator.Core/Entities/Project.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HammerAndStain.Projects.CostCalculator.Core.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ HammerAndStain.Projects.CostCalculator.Core/Entities/ProjectsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace HammerAndStain.Projects.CostCalculator.Core.Entities
+{
+    public class ProjectsContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(ConfigurationManager.ConnectionStrings["ProjectsContext"].ConnectionString);
+    }
+}
